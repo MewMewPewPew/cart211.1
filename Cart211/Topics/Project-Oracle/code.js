46379e7b4c0bf0e1e3bf301cca0498b6a47dd87b
Ash <131301186+MewMewPewPew@users.noreply.github.com>
2025-11-10 17:13:40 -0500

    Title page looks like something
    Made it a bit interactive with a button,
    figured out how to mix all javaScript html and css to do this

---
+++ Cart211/Topics/Project-Oracle/code.js
@@ -0,0 +1,7 @@
+let changeImg = document.getElementById("button");
+changeImg.addEventListener("click", function () {
+  document.getElementById("imgChange").src ="assets/image/Title- PO.png";
+  document.getElementById("button").style.backgroundColor = "rgba(0, 0, 0, 0)";
+  document.getElementById("button").style.color = "rgba(0, 0, 0, 0)";
+  document.getElementById("button").style.border = "rgba(0, 0, 0, 0)";
+});

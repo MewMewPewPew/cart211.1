36c9ea3d8125d49d04ac358cf654b590067965c4
Ash <131301186+MewMewPewPew@users.noreply.github.com>
2025-11-11 00:48:14 -0500

    dibble dabbled

--- Cart211/Topics/Project-Oracle/code.js
+++ Cart211/Topics/Project-Oracle/code.js
@@ -1,10 +1,28 @@
+"use strict";
+
+window.onscroll = function() {
+  console.log("Scroll position: ", window.pageYOffset);
+}
+//title code
 let changeImg = document.getElementById("button");
 changeImg.addEventListener("click", function () {
+  
   document.getElementById("imgChange").src ="assets/image/Title- PO.png";
   document.getElementById("button").style.backgroundColor = "rgba(0, 0, 0, 0)";
   document.getElementById("button").style.color = "rgba(0, 0, 0, 0)";
   document.getElementById("button").style.border = "rgba(0, 0, 0, 0)";
   document.getElementById("textNeed").style.color = "rgb(0, 0, 0)";
   document.getElementById("buttonNeed").style.color = "rgb(0, 0, 0)";
+ 
 
-});
+})
+//privacy code
+let changeText = document.getElementById("button2");
+changeText.addEventListener("click", function () {
+  document.getElementById("answer1").style.color = "rgba(0, 0, 0, 0)";
+  document.getElementById("answer2").style.color = "rgba(0, 0, 0, 0)";
+  document.getElementById("text1").style.color = "rgb(0, 0, 0)";
+  document.getElementById("answer3").style.color = "rgb(0, 0, 0)";
+  
+
+})

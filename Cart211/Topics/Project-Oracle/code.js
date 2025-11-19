9b0fe932f19c1723a59ccf10f5099e8aabbc8634
Ash <131301186+MewMewPewPew@users.noreply.github.com>
2025-11-19 17:29:07 -0500

    Made the conditions on privacy page work

--- Cart211/Topics/Project-Oracle/code.js
+++ Cart211/Topics/Project-Oracle/code.js
@@ -17,6 +17,7 @@ changeImg.addEventListener("click", function () {
 
 })
 //privacy code
+/*
 let changeText = document.getElementById("button2");
 changeText.addEventListener("click", function () {
   document.getElementById("answer1").style.color = "rgba(0, 0, 0, 0)";
@@ -25,5 +26,4 @@ changeText.addEventListener("click", function () {
   document.getElementById("answer3").style.color = "rgb(0, 0, 0)";
 
 
-})
-//wasssup
+})*/
---
+++ Cart211/Topics/Project-Oracle/codeCondition.js
@@ -0,0 +1,17 @@
+"use strict";
+
+window.onscroll = function () {
+  console.log("Scroll position: ", window.pageYOffset);
+}
+
+//privacy code
+let changeText = document.getElementById("button2");
+changeText.addEventListener("click", function () {
+  document.getElementById("button2").style.display = "none";
+  document.getElementById("answer1").style.display = "none";
+  document.getElementById("answer2").style.display = "none";
+  document.getElementById("condition").style.display = "block";
+  document.getElementById("understand").style.display = "block";
+
+
+}) 
